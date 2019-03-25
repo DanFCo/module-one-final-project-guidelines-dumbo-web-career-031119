96614842f85257547c6b49631cceb9a32e2d138a
Jeff Yu <jwy5140@gmail.com>
2019-03-25 15:21:09 -0400

    v1.2 Refined Associations

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -1,3 +1,4 @@
 class Adopter < ActiveRecord::Base
     has_many :dogs
+    has_many :shelters, through: :dogs
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,4 @@
 class Shelter < ActiveRecord::Base
     has_many :dogs
+    has_many :adopters, through: :dogs
 end
